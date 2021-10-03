b7b260717f3ddd2fe7ee162f236a223dd20d7a15
Maral Ashyrova <ashirova310197@gmail.com>
2021-10-04 02:49:44 +0300

    added docstring

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 
 def add_url_params(url: str, params: dict[str, Optional[str]]) -> str:
+    """Helps for combining parameters with url."""
     result = (
         f'{key}={value}'
         for key, value in params.items()
